8011527d6c1d52057ed23ab5d86899419b94e637
Kunta Vyshnavi <135843758+vyshnavikunta@users.noreply.github.com>
2023-12-02 12:30:03 +0530

    Create Number reverse.c

---
+++ C Programming/Number reverse.c
@@ -0,0 +1,42 @@
+/*
+Given a number N, reverse the number.
+
+Input Format
+
+First and only line of input contains a integer - N.
+
+Constraints
+
+-1018 <= N <= 1018
+
+Output Format
+
+Print the reversed number.
+
+Sample Input 0
+
+1344
+Sample Output 0
+
+4431
+*/
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {  
+    
+    long long num,n,rem=0,rev=0;
+    scanf("%lld",&num);
+    n=num;
+    while(n!=0)
+    {
+        rem=n%10;   
+        rev=rev*10+rem;
+        n=n/10;
+    }
+        printf("%lld",rev);   
+
+    return 0;
+}
