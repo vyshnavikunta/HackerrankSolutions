7e6e9fb53ca8dcc471480a977e08c7712708e217
Kunta Vyshnavi <135843758+vyshnavikunta@users.noreply.github.com>
2023-12-02 16:57:53 +0530

    Create Print right-angled triangle.py

---
+++ Python/Print right-angled triangle.py
@@ -0,0 +1,56 @@
+'''
+Print mirror image of right-angled triangle using '*'. See examples for more details.
+
+Input Format
+
+First line of input contains T - number of test cases. Its followed by T lines, each line contains a single integer N - the size of the pattern.
+
+Constraints
+
+1 <= T <= 100
+1 <= N <= 100
+
+Output Format
+
+For each test case, print the test case number as shown, followed by the pattern, separated by newline.
+
+Sample Input 0
+
+4
+2
+1
+5
+10
+Sample Output 0
+
+Case #1:
+ *
+**
+Case #2:
+*
+Case #3:
+    *
+   **
+  ***
+ ****
+*****
+Case #4:
+         *
+        **
+       ***
+      ****
+     *****
+    ******
+   *******
+  ********
+ *********
+**********
+'''
+t = int(input())
+for k in range(1,t+1):
+    print(f"Case #{k}:")
+    n = int(input())
+    for i in range(1,n+1):
+            x = n-i
+            y = i
+            print(" " * x + "*" * y)
